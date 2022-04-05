d9a657f969b9f89436ef8b69a868e211e75a80b7
Salman Khan <94041064+salmankhan2482@users.noreply.github.com>
2022-04-05 09:14:47 +0500

    push

--- resources/views/front-view/pages/include/pagination.blade.php
+++ resources/views/front-view/pages/include/pagination.blade.php
@@ -6,48 +6,46 @@
     $urlstring = http_build_query($geet);
   @endphp
   @if ($paginator->onFirstPage())
-  
   @else
     <li class="page-item">
       <a href="{{ $paginator->previousPageUrl() }}&{{ $urlstring }}" rel="prev" class="nexPre page-link">
-        < Pre </a>
+        < PREV </a>
     </li>
   @endif
 
-    {{-- Pagination Elements --}}
-    @foreach ($elements as $element)
-        {{-- "Three Dots" Separator --}}
-        @if (is_string($element))
-          <li class="page-item disabled">
-            <span class="page-link">{{ $element }}</span>
+  {{-- Pagination Elements --}}
+  @foreach ($elements as $element)
+    {{-- "Three Dots" Separator --}}
+    @if (is_string($element))
+      <li class="page-item disabled">
+        <span class="page-link">{{ $element }}</span>
+      </li>
+    @endif
+
+    {{-- Array Of Links --}}
+    @if (is_array($element))
+      @foreach ($element as $page => $url)
+        @if ($page == $paginator->currentPage())
+          <li class="page-item active">
+            <a href="#" class="page-link">{{ $page }}</a>
+          </li>
+        @else
+          <li class="page-item"><a href="{{ $url }}&{{ $urlstring }}" class="page-link">
+              {{ $page }}
+            </a>
           </li>
         @endif
-
-        {{-- Array Of Links --}}
-        @if (is_array($element))
-          @foreach ($element as $page => $url)
-            @if ($page == $paginator->currentPage())
-              <li class="page-item active">
-                <a href="#" class="page-link">{{ $page }}</a>
-              </li>
-            @else
-              <li class="page-item"><a href="{{ $url }}&{{ $urlstring }}" class="page-link">
-                  {{ $page }}
-                </a>
-              </li>
-            @endif
-          @endforeach
-        @endif
-    @endforeach
+      @endforeach
+    @endif
+  @endforeach
 
   {{-- Next Page Link --}}
   @if ($paginator->hasMorePages())
     <li class="page-item">
       <a href="{{ $paginator->nextPageUrl() }}&featured={{ request()->get('featured') }}" rel="next" class="nexPre page-link">
-        Next <i class="fas fa-chevron-right"></i>
+        NEXT <i class="fas fa-chevron-right"></i>
       </a>
     </li>
   @else
   @endif
-  
 </ul>
