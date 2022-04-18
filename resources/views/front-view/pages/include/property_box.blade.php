d79bf93ca2d5dfe5ce460c19830e872c672913c6
Salman Khan <94041064+salmankhan2482@users.noreply.github.com>
2022-04-18 10:12:44 +0500

    push

--- resources/views/front-view/pages/include/property_box.blade.php
+++ resources/views/front-view/pages/include/property_box.blade.php
@@ -1,79 +1,76 @@
-<a class="single-property-box" href="{{ route('property-detail', [strtolower($property->property_purpose), $property->property_slug, $property->id]) }}">
-
+<a class="single-property-box @if ((new \Jenssegers\Agent\Agent())->isMobile()) horizontal-view @endif"
+    href="{{ route('property-detail', [strtolower($property->property_purpose), $property->property_slug, $property->id]) }}">
+  
     <div class="property-item">
-        <div class="property-img">
-            @if ($property->featured_image)
-                <img src="{{ URL::asset('upload/properties/thumb_' . $property->featured_image) }}" alt="Featured Image of Property">
-            @else
-                <img src="{{ URL::asset('assets/images/icon-no-image.svg') }}" alt="Image of Property if no Featured Image found">
-            @endif
-        </div>
-
-        <ul class="feature_text">
-            @if ($property->featured_property == 1)
-                <li class="feature_cb">
-                    <span>
-                        Featured
-                    </span>
-                </li>
-            @endif
-            <li class="feature_or">
-                <span>
-                    For {{ $property->property_purpose }}
-                </span>
-            </li>
-        </ul>
-
-        <div class="property-author-wrap">
-            <div class="property-author">
-                <span> {{ $property->getPrice() }}
-                    @if ($property->property_purpose == 'For Rent' || $property->property_purpose == 'Rent')
-                        / Month
-                    @endif
-                </span>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="property-title-box" onclick="window.location='{{ url(strtolower($property->property_purpose) . '/' . $property->property_slug . '/' . $property->id) }}';" style="cursor: pointer;">
-
-        <h2 class="property-card__property-title">
-            {{ $property->property_name }}
-        </h2>
-        
-        <ul class="property-feature">
-            <li class="pe-2">
-                <span>{{ Str::limit($property->propertiesTypes->types, 36) }}</span>
-            </li>
-            @if ($property->getProperty_type())
-                <li>
-                    <i class="fas fa-bed"></i>
-                    <span>
-                        {{ $property->bedrooms }}
-                    </span>
-                </li>
-                <li>
-                    <i class="fas fa-bath"></i>
-                    <span>
-                        {{ $property->bathrooms }}
-                    </span>
-                </li>
-            @endif
-
-            <li>
-                <span>
-                    <i class="fas fa-chart-area"></i>
-                    <span>
-                        {{ $property->getSqm() }}
-                    </span>
-                </span>
-            </li>
-        </ul>
-
-        <div class="property-location mt-2">
-            <i class="fa fa-map-marker-alt"></i>
-            <p>{{ Str::limit($property->address, 36) }}</p>
+      <div class="property-img">
+        @if ($property->featured_image)
+          <img src="{{ URL::asset('upload/properties/thumb_' . $property->featured_image) }}" alt="Featured Image of Property">
+        @else
+          <img src="{{ URL::asset('assets/images/icon-no-image.svg') }}" alt="Image of Property if no Featured Image found">
+        @endif
       </div>
+  
+      <ul class="feature_text">
+        @if ($property->featured_property == 1)
+          <li class="feature_cb">
+            <span>
+              Featured
+            </span>
+          </li>
+        @endif
+        <li class="feature_or">
+          <span>
+            For {{ $property->property_purpose }}
+          </span>
+        </li>
+      </ul>
+  
+      @if ((new \Jenssegers\Agent\Agent())->isTablet() || (new \Jenssegers\Agent\Agent())->isDesktop())
+        <div class="property-author-wrap">
+          <div class="property-author">
+            <span> {{ $property->getPrice() }}
+              @if ($property->property_purpose == 'For Rent' || $property->property_purpose == 'Rent')
+                / Month
+              @endif
+            </span>
+          </div>
+        </div>
+      @endif
+  
     </div>
-</a>
+  
+    <div class="property-title-box">
+      <div class="price">
+        {{ $property->getPrice() }}
+  
+        @if ($property->property_purpose == 'For Rent' || $property->property_purpose == 'Rent')
+          / Month
+        @endif
+      </div>
+  
+      <h5 class="property-card__property-title  @if ((new \Jenssegers\Agent\Agent())->isMobile()) text-primary @endif">
+        {{ $property->property_name }}
+      </h5>
+  
+      <ul class="property-feature">
+        @if ($property->getProperty_type())
+          <li><i class="fas fa-bed"></i>
+            <span>{{ $property->bedrooms }}</span>
+          </li>
+          <li><i class="fas fa-bath"></i>
+            <span>{{ $property->bathrooms }}</span>
+          </li>
+        @endif
+        <li><i class="fas fa-chart-area"></i>
+          <span>{{ $property->getSqm() }}</span>
+        </li>
+      </ul>
+  
+      <div class="property-location">
+        <i class="fa fa-map-marker-alt"></i>
+        <p>{{ Str::limit($property->address, 36) }}</p>
+      </div>
+  
+    </div>
+  </a>
+  
--- resources/views/front/pages/include/featured_properties.blade.php
+++ resources/views/front/pages/include/featured_properties.blade.php
@@ -1,264 +1,44 @@
-<style>
-    @media (max-width: 767px){
-        .desktopFeaturesPropsLis{
-            display: none;
-        }
-
-        .trending-places{
-            padding-top: 10px !important;
-        }
-        .text-justify{
-            padding-bottom: 10px !important;
-        }
-    }
-    @media only screen and (min-width: 800px) {
-        
-        .liDeskAreaSpan{
-            float: left;
-            margin-left: 15px;
-        }
-
-        .property-feature .liDesk{
-            margin-left: -15px;
-        }
-        .property-item img{
-            min-height: 250px !important;
-        }
-    }   
-</style>
-
-<div class="trending-places pb-50 bg-light pt-50">
+<div class="bg-light py-5">
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="section-title text-center">
-                    <h2>Featured Properties</h2>
-                    <p class="text-justify">Saakin is the fastest-growing real estate directory in Qatar. 
-                        You can find featured properties on the website, 
-                        and you discover all the updates of the real estate market of Qatar through the listings. 
-                        Moreover, the portal has managed an amazing platform to connect buyers and sellers with real estate agencies.
-                        <span id="dots">...</span>
-                        <span id="more"> 
-                            We respond to you quickly to connect with the agents to provide you with the best properties. 
-                            You find properties for Sale and Rent on the website, 
-                            and you get multiple choices to have the selling and buying properties in Qatar. 
-                            Whether you are searching for apartments for living or commercial properties for investment purposes, 
-                            we help you with fast assistance. Saakin inc tries its best to bring you the most desirable and ideal property. 
-                            You can have information on the specifications of the featured properties and prices through our portal.
-                        </span>
-                        <button onclick="myFunction()"  class="myBtn btn btn-sm btn-info-outline">Read more</button></p>
-                </div>
-            </div>
-            <!-- Add Arrows -->
-            <div class="container desktopFeaturesPropsLis">
-                <div class="tab-pane fade show active property-grid" id="grid-view">
-                    <div class="row row-10-padding">
-                        @foreach ($featured_properties as $property)
-                            <div class="col-md-6 col-lg-4 col-sm-12">
-                                @include('front.pages.include.property_box')
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="col-md-12">
-                <div class="card-list featured_mobile">
-                    <div class="card-list__item">
-                        <div class="property-card__section">
-                            @if (count($featured_properties) > 0)
-
-                            @foreach ($featured_properties as $property)
-
-                                    <div class="property-card">
-                                        <div class="property-card__image">
-                                            <div class="swiper-container">
-                                                <div class="swiper-wrapper">
-                                                    @if (count($property->gallery) > 0)
-                                                        <div class="swiper-slide">
-                                                            <img src="{{ asset('upload/properties/thumb_' . $property->featured_image) }}" 
-                                                            alt="{{$property->property_name}}">
-                                                        </div>
-                                                        @foreach ($property->gallery as $gallery)
-                                                            @if($loop->index < 2)
-                                                            <div class="swiper-slide">
-                                                                <img src="{{ asset('upload/gallery/') . '/' . $gallery->image_name }}" 
-                                                                alt="{{$property->property_name}}">
-                                                            </div>
-                                                            @endif
-                                                        @endforeach
-                                                    @endif
-                                                </div>
-                                                <!-- Add Pagination -->
-                                                <div class="swiper-pagination"></div>
-                                            </div>
-                                        </div>
-                                        <div class="property-card__content">
-                                            <div class="property-card__info-area"
-                                                onclick="window.location
-                                       ='{{ url(strtolower($property->property_purpose) . '/' . $property->property_slug . '/' . $property->id) }}';">
-                                                <div class="property-card__title ">
-                                                    <a class="property-card__title-link" href="javascript:;">
-                                                        {{ $property->getPrice() }} @if ($property->property_purpose == 'For Rent' || $property->property_purpose == 'Rent')
-                                                            / Month
-                                                        @endif
-                                                    </a>
-
-                                                </div>
-                                                <h2 class="property-card__property-title">
-                                                    {{ \Illuminate\Support\Str::limit($property->property_name, 20) }}
-                                                </h2>
-                                                <div class="property-card__location">
-                                                    {{ $property->propertiesTypes->types }}
-                                                    <br>
-                                                    {{ \Illuminate\Support\Str::limit($property->address, 30) }}
-                                                </div>
-                                                <div class="property-card__info ">
-                                                    @if ($property->getProperty_type())
-                                                        <i class="fas fa-bed"></i> <span>{{ $property->bedrooms }}
-                                                        </span>
-                                                        <i class="fas fa-bath"></i>
-                                                        <span>{{ $property->bathrooms }}
-                                                        </span>
-                                                    @endif
-                                                    <i class="fas fa-chart-area"></i>
-                                                    <span>{{ $property->getSqm() }}</span>
-                                                </div>
-                                            </div>
-                                            <div class="property-card__actions mt-0">
-
-@php
-$phone = \App\Properties::getPhoneNumber($property->id);
-$whatsapp = \App\Properties::getWhatsapp($property->id);
-$agency = \App\Agency::where("id",$property->agency_id)->first();
-$propertyUrl = url(strtolower($property->property_purpose) . '/' . $property->property_slug . '/' . $property->id);
-$whatsapText = 'Hello,
-I would like to inquire about this property posted on
-saakin.qa
-
-Reference: '.$property->refference_code.'
-Price: QR '.$property->getPrice().'/month
-Type: '.$property->propertiesTypes->types.'
-Location: '.$property->address.'
-
-Link:'.$propertyUrl;
-@endphp
-
-                                                @if ($phone != '#')
-                                                    <a class="btn btn-outline-success call_btn"
-                                                        href="tel:{{ $phone }}">
-                                                        <i class="fas fa-phone-alt"></i> Call
-                                                    </a>
-                                                @endif
-                                                @if ($whatsapp != '#' && $whatsapp != '')
-                                                    <a href="//api.whatsapp.com/send?phone={{ $whatsapp }}&text={{ urlencode(trim($whatsapText)) }}"
-                                                        class="btn btn-success call_btn">
-                                                        <i class="fab fa-whatsapp"></i>
-                                                        WhatsApp
-                                                    </a>
-                                                @else
-                                                <a class="btn btn-outline-success call_btn" 
-                                                id="emailBtn"
-                                                    data-toggle="modal"
-                                                    data-target="#exampleModal"
-                                                    data-image="{{asset('upload/properties/thumb_' . $property->featured_image) }}"
-                                                    data-title="{{ $property->property_name }}"
-                                                    data-agent="{{ $property->agent_name ?? $agency->name }}"
-                                                    data-broker="{{ $agency->name ?? '' }}"
-                                                    data-bedroom="{{ $property->bedrooms ?? '' }}"
-                                                    data-bathroom="{{ $property->bathrooms ?? '' }}"
-                                                    data-area="{{ $property->getSqm() ?? '' }}"
-                                                >
-                                                    <i class="fas fa-envelope"></i> Email
-                                                </a>
-                                                @endif
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                @endforeach
-                            @endif
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <div class="text-center">
+            <h2 class="h1 mb-3">Featured Properties</h2>
+            <p class="text-justify">Saakin is the fastest growing Qatar Real Estate Directory you can find Properties for
+              Rent in Qatar. For apartments on rent real estate agents in Qatar bring you the best apartments in Doha,
+              <span id="dots">...</span>
+              <span id="more" style="display: none">
+                The Pearl, West Bay, and properties from all over the country thourgh Qatar property
+                directory.
+              </span>
+              <a class="btn btn-primary btn-sm" href="javascript:void(0)" onclick="myFunction()">Read more</a>
+            </p>
+          </div>
         </div>
-        <div class="row mt-5">
-
-            <div class="col-md-3 text-center mt-2 ml-auto mr-auto">
-                <a href="{{ route('featured-properties') }}" class="btn v3">See All Featured Properties</a>
-            </div>
+      </div>
+  
+      <!-- Add Arrows -->
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="row gy-3 gy-md-4 mt-1">
+            @foreach ($featured_properties as $property)
+              <div class="col-md-6">
+                @include('front.pages.include.property_box')
+              </div>
+            @endforeach
+          </div>
         </div>
+        <div class="col-lg-4">
+          <div></div>
+        </div>
+      </div>
+  
+      <div class="row justify-content-center mt-3 mt-md-5">
+        <div class="col-md-3">
+          <a href="{{ route('featured-properties') }}" class="btn btn-primary w-100">See All Featured Properties</a>
+        </div>
+      </div>
     </div>
-
-</div>
-
-
-
-<script src="{{asset('assets/js/swiper.js')}}"></script>
-<script src="{{asset('assets/js/Jquery.min.js')}}"></script>
-        
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.4/dist/sweetalert2.all.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0.0/dist/fancybox.umd.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0.0/dist/carousel.autoplay.umd.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0.0/dist/carousel.umd.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0.0/dist/panzoom.umd.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0.0/dist/panzoom.controls.umd.js"></script>
-<script>
-
-    function FormSubmit(coming) {
-        var value = coming.value;
-        $("#sort_by").val(value);
-        document.getElementById('frmSortBy').submit();
-    }
-
-    //DOTS SLIDER
-    var newswiper = new Swiper('.related-homes-slider', {
-        slidesPerView: 3,
-        spaceBetween: 26,
-
-        dots: true,
-        slidesPerColumn: 2,
-        grid: {
-            rows: 2,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        breakpoints: {
-            767: {
-                slidesPerView: 1,
-
-            },
-            991: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            }
-        }
-    });
-    
-    //DOTS SLIDER
-    $(document).ready(function() {
-        var width = $(window).width();
-        console.log(width)
-        if (width < 768) {
-            var swiper = new Swiper('.swiper-container', {
-                loop: true,
-                dots: true,
-
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                    dynamicBullets: true,
-                    dynamicMainBullets: 1,
-                },
-            });
-        }
-    });
-
-    </script>
-
-
-
+  </div>
+  </div>
+  
