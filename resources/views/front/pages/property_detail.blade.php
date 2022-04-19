b98b06bbe2491ccc579ef400b5f9a7edad386d8a
Salman Khan <94041064+salmankhan2482@users.noreply.github.com>
2022-04-19 15:11:08 +0500

    push

--- resources/views/front/pages/property_detail.blade.php
+++ resources/views/front/pages/property_detail.blade.php
@@ -253,7 +253,9 @@
                     <div class="rounded-img">
                       <!-- agent pic on desktop -->
                       @if (!empty($property->agent_picture))
+
                         <a class="d-block" href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] ) }}">
+
                           <img src="{{ asset('upload/properties/' . $property->agent_picture) }}" alt="{{ $property->agent_name }}" width="120">
                         </a>
                       @else
@@ -268,19 +270,18 @@
                       <h5>{{ $property->agent_name }}</h5>
                       <div class="grid mt-3" style="--template: 50px auto; --gap: .5rem">
                         <div>
-                          <a href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] ) }}">
+
+                          <a href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] )}}">
                             <img src="{{ asset('upload/agencies/' . $agency->image) }}" class="rounded-circle" alt="{{ $property->agent_name }}" width="50">
                           </a>
                         </div>
                         <div>
-                          <a class="link-dark text-decoration-none fw-bold" 
-                            href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] ) }}">{{ $agency->name }}
+                          <a class="link-dark text-decoration-none fw-bold" href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] ) }}">
+                            {{ $agency->name }}
                           </a>
                           <br>
-                          <a class="link-dark text-decoration-none fw-bold" 
-                            href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] ) }}">
-                            ({{ count( App\Properties::where('status', '1')->where('agency_id', $agency->id)->get(), ) }}
-                            properties)
+                          <a class="link-dark text-decoration-none fw-bold"  href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] ) }}">
+                            ({{ App\Properties::where('status', '1')->where('agency_id', $agency->id)->count()  }} Properties)
                           </a>
                         </div>
                       </div>
@@ -298,15 +299,13 @@
                     <div class="d-md-block">
                       @if (!empty($agency->name))
                         <h5>
-                          <a class="link-dark text-decoration-none fw-bold" href="{{ url('agency/' . Str::slug($agency->name, ' -') . '/' . $agency->id) }}">
+                          <a class="link-dark text-decoration-none fw-bold" href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] ) }}">
                             {{ $agency->name }}
                           </a>
                         </h5>
                         <a class="link-dark text-decoration-none" href="{{ route('agency_detail', [Str::slug($agency->name),$agency->id] ) }}">
-                          ({{ count(
-                              App\Properties::where('status', '1')->where('agency_id', $agency->id)->get(),
-                          ) }}
-                          properties)</a>
+                          ({{ App\Properties::where('status', '1')->where('agency_id', $agency->id)->count()}} Properties)
+                        </a>
                       @else
                         Agent not found
                       @endif
@@ -480,7 +479,7 @@
                   Facebook</a>
               </div>
               <div>
-                <a href="https://twitter.com/share?url={{url()->current()}}" class="btn btn-monochrome btn-sm share-btn">
+                <a href="https://pinterest.com/pin/create/button/?url={{url()->current()}}" class="btn btn-monochrome btn-sm share-btn">
                   <svg viewBox="0 0 22 20" class="btn-icon" style="--icon-color: #e60019;">
                     <path
                       d="M10.008 0C4.481 0 0 4.474 0 9.992c0 4.235 2.636 7.853 6.36 9.309-.091-.79-.166-2.007.032-2.87.181-.781 1.17-4.967 1.17-4.967s-.297-.6-.297-1.48c0-1.39.807-2.426 1.812-2.426.857 0 1.269.641 1.269 1.406 0 .855-.544 2.138-.832 3.33-.239.995.502 1.81 1.483 1.81 1.779 0 3.146-1.875 3.146-4.573 0-2.393-1.721-4.062-4.184-4.062-2.85 0-4.522 2.13-4.522 4.334 0 .855.329 1.776.74 2.278.083.098.092.189.067.287-.074.313-.247.995-.28 1.135-.041.181-.149.222-.338.132-1.252-.584-2.035-2.401-2.035-3.873 0-3.15 2.29-6.045 6.615-6.045 3.468 0 6.17 2.467 6.17 5.773 0 3.446-2.175 6.217-5.19 6.217-1.013 0-1.969-.526-2.29-1.151l-.626 2.377c-.222.871-.832 1.957-1.244 2.623.94.288 1.928.444 2.966.444C15.519 20 20 15.526 20 10.008 20.016 4.474 15.535 0 10.008 0z">
@@ -489,7 +488,7 @@
                   Pinterest</a>
               </div>
               <div>
-                <a href="https://api.whatsapp.com/send?text={{url()->current()}}" class="btn btn-monochrome btn-sm share-btn">
+                <a href="https://twitter.com/share?url={{url()->current()}}" class="btn btn-monochrome btn-sm share-btn">
                   <svg viewBox="0 0 22 20" class="btn-icon" style="--icon-color: #1da1f3;">
                     <path
                       d="M20 10c0 5.525-4.475 10-10 10S0 15.525 0 10 4.475 0 10 0s10 4.475 10 10zM7.843 15.79c4.373 0 6.763-4.051 6.763-7.562 0-.116 0-.231-.004-.342a5.228 5.228 0 0 0 1.187-1.377c-.423.21-.882.352-1.365.419.493-.331.868-.85 1.045-1.472a4.519 4.519 0 0 1-1.508.645c-.434-.518-1.05-.838-1.735-.838-1.312 0-2.376 1.19-2.376 2.657 0 .209.02.413.064.606-1.977-.11-3.727-1.169-4.9-2.778a2.91 2.91 0 0 0-.32 1.334c0 .92.419 1.736 1.06 2.21a2.15 2.15 0 0 1-1.075-.33v.032c0 1.29.818 2.359 1.907 2.607a2.136 2.136 0 0 1-1.074.044c.3 1.058 1.178 1.824 2.218 1.846-.813.711-1.839 1.136-2.953 1.136-.192 0-.38-.011-.566-.039a6.13 6.13 0 0 0 3.632 1.201z">
@@ -498,11 +497,11 @@
                   Twitter</a>
               </div>
               <div>
-                <a href="https://pinterest.com/pin/create/button/?url={{url()->current()}}" class="btn btn-monochrome btn-sm share-btn"><svg viewBox="0 0 22 20" class="btn-icon" style="--icon-color: #ef5e4e;">
-                    <path
-                      d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10zm5.259-14.966A1.001 1.001 0 0 0 15 5H5c-.09 0-.176.012-.259.034l4.552 4.552a1 1 0 0 0 1.414 0l4.552-4.552zm.707.707l-4.552 4.552a2 2 0 0 1-2.828 0L4.034 5.74C4.012 5.824 4 5.911 4 6v7a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V6c0-.09-.012-.176-.034-.259z">
-                    </path>
-                  </svg> Email</a>
+                <a href="https://api.whatsapp.com/send?text={{url()->current()}}" class="btn btn-monochrome btn-sm share-btn">
+                  <svg viewBox="05 06 22 20" class="btn-icon" style="--icon-color: #25d366;">
+                  <path 
+                    d=" M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z"></path>
+                  </svg> WhatsApp</a>
               </div>
             </div>
           </div>
@@ -720,40 +719,48 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <div class="d-flex flex-wrap align-items-center spbwx12 spbwy8">
-              <div>
-                <a href="#!" class="btn btn-monochrome btn-sm share-btn">
+            <div class="d-flex flex-wrap text-center justify-content-center">
+              <div class="col-5 m-1">
+                <a href="http://www.facebook.com/sharer.php?u={{url()->current()}}" class="btn btn-monochrome share-btn w-100">
                   <svg viewBox="0 0 22 20" class="btn-icon" style="--icon-color: #4267b2;">
                     <path
                       d="M20 10.061C20 4.505 15.523 0 10 0S0 4.505 0 10.061C0 15.083 3.657 19.245 8.438 20v-7.03h-2.54V10.06h2.54V7.845c0-2.522 1.492-3.915 3.777-3.915 1.094 0 2.238.197 2.238.197v2.476h-1.26c-1.243 0-1.63.775-1.63 1.57v1.888h2.773l-.443 2.908h-2.33V20c4.78-.755 8.437-4.917 8.437-9.939">
                     </path>
                   </svg>
-                  Facebook</a>
+                  Facebook
+                </a>
               </div>
-              <div>
-                <a href="#!" class="btn btn-monochrome btn-sm share-btn">
-                  <svg viewBox="0 0 22 20" class="btn-icon" style="--icon-color: #e60019;">
-                    <path
-                      d="M10.008 0C4.481 0 0 4.474 0 9.992c0 4.235 2.636 7.853 6.36 9.309-.091-.79-.166-2.007.032-2.87.181-.781 1.17-4.967 1.17-4.967s-.297-.6-.297-1.48c0-1.39.807-2.426 1.812-2.426.857 0 1.269.641 1.269 1.406 0 .855-.544 2.138-.832 3.33-.239.995.502 1.81 1.483 1.81 1.779 0 3.146-1.875 3.146-4.573 0-2.393-1.721-4.062-4.184-4.062-2.85 0-4.522 2.13-4.522 4.334 0 .855.329 1.776.74 2.278.083.098.092.189.067.287-.074.313-.247.995-.28 1.135-.041.181-.149.222-.338.132-1.252-.584-2.035-2.401-2.035-3.873 0-3.15 2.29-6.045 6.615-6.045 3.468 0 6.17 2.467 6.17 5.773 0 3.446-2.175 6.217-5.19 6.217-1.013 0-1.969-.526-2.29-1.151l-.626 2.377c-.222.871-.832 1.957-1.244 2.623.94.288 1.928.444 2.966.444C15.519 20 20 15.526 20 10.008 20.016 4.474 15.535 0 10.008 0z">
-                    </path>
-                  </svg>
-                  Pinterest</a>
-              </div>
-              <div>
-                <a href="#!" class="btn btn-monochrome btn-sm share-btn">
+              <div class="col-5 m-1">
+                <a href="https://twitter.com/share?url={{url()->current()}}" class="btn btn-monochrome share-btn w-100">
                   <svg viewBox="0 0 22 20" class="btn-icon" style="--icon-color: #1da1f3;">
                     <path
                       d="M20 10c0 5.525-4.475 10-10 10S0 15.525 0 10 4.475 0 10 0s10 4.475 10 10zM7.843 15.79c4.373 0 6.763-4.051 6.763-7.562 0-.116 0-.231-.004-.342a5.228 5.228 0 0 0 1.187-1.377c-.423.21-.882.352-1.365.419.493-.331.868-.85 1.045-1.472a4.519 4.519 0 0 1-1.508.645c-.434-.518-1.05-.838-1.735-.838-1.312 0-2.376 1.19-2.376 2.657 0 .209.02.413.064.606-1.977-.11-3.727-1.169-4.9-2.778a2.91 2.91 0 0 0-.32 1.334c0 .92.419 1.736 1.06 2.21a2.15 2.15 0 0 1-1.075-.33v.032c0 1.29.818 2.359 1.907 2.607a2.136 2.136 0 0 1-1.074.044c.3 1.058 1.178 1.824 2.218 1.846-.813.711-1.839 1.136-2.953 1.136-.192 0-.38-.011-.566-.039a6.13 6.13 0 0 0 3.632 1.201z">
                     </path>
                   </svg>
-                  Twitter</a>
+                  Twitter
+                </a>
               </div>
-              <div>
-                <a href="#!" class="btn btn-monochrome btn-sm share-btn"><svg viewBox="0 0 22 20" class="btn-icon" style="--icon-color: #ef5e4e;">
+              
+              <div class="col-5 m-1">
+                <a href="https://pinterest.com/pin/create/button/?url={{url()->current()}}" class="btn btn-monochrome share-btn w-100">
+                  <svg viewBox="0 0 22 20" class="btn-icon" style="--icon-color: #e60019;">
                     <path
-                      d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10zm5.259-14.966A1.001 1.001 0 0 0 15 5H5c-.09 0-.176.012-.259.034l4.552 4.552a1 1 0 0 0 1.414 0l4.552-4.552zm.707.707l-4.552 4.552a2 2 0 0 1-2.828 0L4.034 5.74C4.012 5.824 4 5.911 4 6v7a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V6c0-.09-.012-.176-.034-.259z">
+                      d="M10.008 0C4.481 0 0 4.474 0 9.992c0 4.235 2.636 7.853 6.36 9.309-.091-.79-.166-2.007.032-2.87.181-.781 1.17-4.967 1.17-4.967s-.297-.6-.297-1.48c0-1.39.807-2.426 1.812-2.426.857 0 1.269.641 1.269 1.406 0 .855-.544 2.138-.832 3.33-.239.995.502 1.81 1.483 1.81 1.779 0 3.146-1.875 3.146-4.573 0-2.393-1.721-4.062-4.184-4.062-2.85 0-4.522 2.13-4.522 4.334 0 .855.329 1.776.74 2.278.083.098.092.189.067.287-.074.313-.247.995-.28 1.135-.041.181-.149.222-.338.132-1.252-.584-2.035-2.401-2.035-3.873 0-3.15 2.29-6.045 6.615-6.045 3.468 0 6.17 2.467 6.17 5.773 0 3.446-2.175 6.217-5.19 6.217-1.013 0-1.969-.526-2.29-1.151l-.626 2.377c-.222.871-.832 1.957-1.244 2.623.94.288 1.928.444 2.966.444C15.519 20 20 15.526 20 10.008 20.016 4.474 15.535 0 10.008 0z">
                     </path>
-                  </svg> Email</a>
+                  </svg>
+                  Pinterest
+                </a>
+              </div>
+              
+              <div class="col-5 m-1">
+                <a href="https://api.whatsapp.com/send?text={{url()->current()}}" class="btn btn-monochrome share-btn w-100">
+                  <svg viewBox="05 06 22 20" class="btn-icon" style="--icon-color: #25d366;">
+                    <path 
+                      d=" M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z">
+                    </path>
+                  </svg>
+                  WhatsApp
+                </a>
               </div>
             </div>
           </div>
@@ -767,7 +774,7 @@
 
 @push('styles')
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
-
+ 
   <!-- Add the slick-theme.css if you want default styling -->
   <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/slick/slick.css') }}" />
   <!-- Add the slick-theme.css if you want default styling -->
