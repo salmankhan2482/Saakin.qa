8c81875d0c8db594b0b163d8d657c8efb496562c
isalman-khan <roughdata2482@gmail.com>
2022-06-30 23:27:42 +0500

    dom

--- resources/views/front/layouts/jsscripts.blade.php
+++ resources/views/front/layouts/jsscripts.blade.php
@@ -1,7 +1,4 @@
-{{-- @if (Route::currentRouteName() != 'home') --}}
 <script src="{{ asset('assets/js/Jquery.min.js') }}"></script>
-{{-- @endif --}}
-
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
 </script>
 
